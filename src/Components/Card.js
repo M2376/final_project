88858f1862d8d01384aa20a5036370539c1c2d2e
M2376 <87819975+M2376@users.noreply.github.com>
2022-03-29 12:25:50 -0400

    Day2Changes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
-function Example() {
+const styling = {
+  width: "18rem"
+};
+
+function Card(props) {
+  console.log(props);
   return (
-    <div className="Example">
-      <h3>Hello example component!</h3>
+    <div class="card" style={styling}>
+      <img src="..." class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{props.name}</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
     </div>
   );
 }
 
-export default Example;
+export default Card;
